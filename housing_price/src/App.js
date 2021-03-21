f3c8287ef123cea92cc050350d6f8ed8575181b2
vishvam_amin <vishvamamin55@gmail.com>
2021-03-21 13:19:31 -0400

    Data

--- housing_price/src/App.js
+++ housing_price/src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Tensorflow from "./components/Tensorflow.js"
+import Housing from "./components/housing_price.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header> */}
+
+      {/* <Tensorflow /> */}
+      <Housing/>
     </div>
   );
 }
---
+++ housing_price/src/components/housing_price.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Housing_price = () => {
+    return(
+        <div>
+            <h1>welcome</h1>
+        </div>
+    )
+}
+
+export default Housing_price;
